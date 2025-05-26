df5f7e45dfce4e6d3781a4daba7a99aaf81b75b7
notjustsomesmalltowngirl <olukayodepeace2006@gmail.com>
2025-05-26 23:17:04 +0100

    using postgres

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 
 db = SQLAlchemy(model_class=Base, )
 db.init_app(app)
